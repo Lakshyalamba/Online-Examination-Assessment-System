3d659ba439555cfba2fbbba213727917e805f5c8
Ravleen Singh <officialravleensingh@gmail.com>
2026-04-12 11:09:15 +0530

    feat: add objective grading strategies

--- src/modules/evaluation/index.ts
+++ src/modules/evaluation/index.ts
@@ -1,4 +1,5 @@
 export * from "./domain/evaluation.contracts";
 export * from "./domain/result-state";
 export * from "./domain/scoring.types";
+export * from "./utils/objective-grading";
 export * from "./utils/scoring";
